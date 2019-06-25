f2362538007856feb76d3ee3eb84eba26901374c
Will Daskam <willd2122@gmail.com>
2019-06-25 22:20:44 +0000

    added devise gem

--- Gemfile
+++ Gemfile
@@ -70,3 +70,4 @@ end
 gem 'will_paginate', '> 3.0'
 gem 'simple_form'
 gem "font-awesome-rails"
+gem 'devise'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+config.action_mailer.default_url_options = { host: 'node22.codenvy.io:35417' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
