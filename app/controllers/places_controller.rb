ccfe82b5c711f9c10a222061e2bc469b8f963999
Will Daskam <willd2122@gmail.com>
2019-06-26 22:57:44 +0000

    Added validation errors

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
   
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def show
@@ -34,7 +38,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
     end
   
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   
   def destroy
